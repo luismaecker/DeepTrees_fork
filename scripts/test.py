fix: fixed bug in normalization that caused NDVI to be too small

scripts/test.py
@@ -17,7 +17,8 @@ https://github.com/AWF-GAUG/TreeCrownDelineation (v0.1.0). (c) Max Freudenberg, 
 
 import warnings
 import logging
-import os
+
+import geopandas as gpd
 
 import torch
 from lightning import Trainer, seed_everything
@@ -40,6 +41,7 @@ rootutils.set_root(
 
 from treecrowndelineation.model.tcd_model import TreeCrownDelineationModel
 from treecrowndelineation.dataloading.datamodule import TreeCrownDelineationDataModule
+from treecrowndelineation.modules import utils
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 log = logging.getLogger(__name__)
@@ -73,20 +75,31 @@ def train(config: DictConfig) -> None:
     log.info('Instantiating model...')
     model: TreeCrownDelineationModel = hydra.utils.instantiate(config.model)
 
-    if config['pretrained']['model'] is not None:
-        pretrained_model = torch.jit.load(os.path.join(config['pretrained']['path'],
-                                                       config['pretrained']['model']))
+    if config['pretrained_model'] is None:
+        raise ValueError("Inference requires a pretrained model")
+    else:
+        pretrained_model = torch.jit.load(config['pretrained_model'])
         model.load_state_dict(pretrained_model.state_dict())
         log.info('Loaded state dict from pretrained model')
-    else:
-        raise ValueError("Inference requires a pretrained model")
 
     trainer: Trainer = hydra.utils.instantiate(config.trainer, callbacks=callbacks)
 
     log.info('Starting predictions ...')
     output_dict = trainer.predict(model, data)
+
+    all_polygons = []
     for dd in output_dict:
-        print(dd)
+        all_polygons.extend(dd['polygons'])
+
+    log.info(f'Saving all polygons to {config["polygon_file"]}.')
+    utils.save_polygons(all_polygons, config['polygon_file'], crs=config['crs'])
+
+    # additional post processing that works on all polygons
+    baumkataster = gpd.read_file(config['baumkataster_file']).to_crs(config['crs'])
+    inters = gpd.GeoDataFrame(all_polygons).sjoin(baumkataster)
+    log.info(f'Saving all polygons that overlap with Baumkataster to {config["polygon_baumkataster_file"]}.')
+    utils.save_polygons(inters, config['polygon_baumkataster_file'], crs=config['crs'])
+
 
 if __name__=='__main__':
     train()

treecrowndelineation/dataloading/datasets.py
@@ -54,7 +54,7 @@ class TreeCrownDelineationBaseDataset(ABC):
 
         self.raster_files = raster_files
         self.target_files = target_files
-        self.divide_by = divide_by # TODO move this to torchvision transform
+        self.divide_by = divide_by
         self.augmentation = augmentation
         self.ndvi_config = ndvi_config
         self.dilate_outlines = dilate_outlines
@@ -107,13 +107,10 @@ class TreeCrownDelineationBaseDataset(ABC):
                     raster_transforms.append(v2.Pad(**val))
                 case _:
                     raise ValueError(f'Augmentation not defined: {key}')
-        raster_transforms.append(v2.ToDtype(dtype=torch.float32))
-        # apply scaling by constant value as part of the torchvision transform chain
-        ln = lambda x: x/self.divide_by
         if len(joint_transforms) == 0:
             lc = lambda x: x
             joint_transforms.append(v2.Lambda(lc))
-        raster_transforms.append(v2.Lambda(ln))
+        raster_transforms.append(v2.ToDtype(dtype=torch.float32))
         self.augment_joint = v2.Compose(joint_transforms)
 
         if len(target_transforms) > 0:
@@ -142,6 +139,9 @@ class TreeCrownDelineationBaseDataset(ABC):
 
         if used_bands is not None:
             raster = raster.isel(bands=used_bands)
+
+        # need to normalize before concatenating NDVI
+        raster = raster / self.divide_by
 
         if self.ndvi_config['concatenate']:
             raster = self.concatenate_ndvi_to_raster(raster,
@@ -346,6 +346,7 @@ class TreeCrownDelineationInferenceDataset(TreeCrownDelineationBaseDataset, Data
         Returns:
             _type_: _description_
         '''
+        log.info(f'Predicting on {self.raster_files[idx]}')
         if self.in_memory: # retrieve preloaded tiles
             raise NotImplementedError('Inference only works with loading data on-the-fly')
         else: # load from disk