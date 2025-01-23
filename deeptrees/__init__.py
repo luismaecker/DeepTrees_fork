8c7887b752298f0648e890eef394dee408c6a407
Harsh Grover <grover@dkrz.de>
2025-01-23 12:56:53 +0100

    Harsh predict function

--- deeptrees/__init__.py
+++ deeptrees/__init__.py
@@ -6,3 +6,15 @@ from . import modules
 from . import dataloading
 
 from .model.deeptrees_model import TreeCrownDelineationModel
+
+from deeptrees.inference import TreeCrownPredictor
+
+def predict(image_path: list[str]):
+    """
+    Run tree crown delineation prediction on the provided image path.
+
+    Args:
+        image_path (list[str]): List of image paths to process.
+    """
+    predictor = TreeCrownPredictor(image_path=image_path)  # Uses default config path and name
+    predictor.predict()
