5cda9d436970d144b642c353ac2a40e2e5606e6f
Harsh Grover <grover@dkrz.de>
2025-02-17 16:52:45 +0100

    Removing a file that is not needed

--- calling_predict.py
+++
@@ -1,3 +0,0 @@
-from deeptrees import predict
-
-predict(image_path=["/work/ka1176/shared_data/2024-ufz-deeptree/polygon-labelling/pool_tiles/tile_10_5.tif", "/work/ka1176/shared_data/2024-ufz-deeptree/polygon-labelling/pool_tiles/tile_10_6.tif", "/work/ka1176/shared_data/2024-ufz-deeptree/polygon-labelling/pool_tiles/tile_10_7.tif"], config_path="./config/inference_on_individual_tiles.yaml")
