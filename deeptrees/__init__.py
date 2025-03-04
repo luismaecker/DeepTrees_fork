a81d925465368bc4fb22236deaf72a402962f5f6
Harsh Grover <grover@dkrz.de>
2025-03-04 14:24:42 +0100

    refactor: Remove download pretrained function

--- deeptrees/__init__.py
+++ deeptrees/__init__.py
@@ -26,18 +26,3 @@ def predict(image_path: list[str], config_path: str):
     """
     predictor = TreeCrownPredictor(image_path=image_path, config_path=config_path)  # Uses default config path and name
     predictor.predict()
-
-
-def download_pre_trained_model_weights():
-    """
-    Downloads the pretrained model weights for the Freudenberg et al. 2022 model.
-    
-    Args:
-        download_path (str): The path to save the downloaded model weights to.
-    """
-    
-    os.makedirs('./pretrained_models', exist_ok=True)
-    
-    file_name = os.path.join('./pretrained_models', "lUnet-resnet18_epochs=209_lr=0.0001_width=224_bs=32_divby=255_custom_color_augs_k=3_jitted.pt")
-    
-    freudenberg2022(file_name)
